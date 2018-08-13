2f40d053b0e46f827f11bf8968e2c916dd24800f
root <root@hofmanjosh555@gmail.com>
2018-08-12 19:09:03 -0700

    commit 1

---
+++ Repeated-key-press.bat
@@ -0,0 +1,22 @@
+@echo off
+set CURR=%TIME%
+:start
+set /a COUNT=COUNT+1
+set CLOCK=%TIME%
+ 
+pushd %~dp0
+cscript VBS_key.vbs
+cls
+echo --------------------------------------
+echo This Script was started at %CURR%
+echo --------------------------------------
+echo It is currently %CLOCK%
+echo --------------------------------------
+echo This Script has looped %COUNT% times
+echo --------------------------------------
+echo Hit a key!
+echo --------------------------------------
+ 
+pause>null
+cls
+goto start
